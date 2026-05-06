4540c98c46d94d6ad81be950848e3d764e902929
gabriexlss <gabrielsasukeclash@gmail.com>
2026-05-06 12:26:14 -0300

    alterações do vitor

--- backend/server.js
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./backend/.env" }); //confirmando que o path do env ta corrento porque ele so nao carregava antes
-const { default: app } = await import("./src/app.js"); //precissei por esse await porque ele so carregava as coisas fora de ordem entao o banco de dados simplesmente nao funcionava
-
-app.listen(3001);
