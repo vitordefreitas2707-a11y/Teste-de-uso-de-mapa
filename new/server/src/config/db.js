6e9f99a766e392f0dc0c91d23d42eb88968bd85f
gabriexlss <gabrielsasukeclash@gmail.com>
2026-05-06 12:23:41 -0300

    Revert "Alteração da Estrutura das pastas"
    This reverts commit ddfe4432d0d2ecc9a139263552c7d66fb5ab4200.

--- new/server/src/config/db.js
+++
@@ -1,9 +0,0 @@
-import pkg from "pg";
-const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
-export default pool;
